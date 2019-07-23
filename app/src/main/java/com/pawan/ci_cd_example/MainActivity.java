747e0dd42429b5c0e0ac7520daca5473a8f4bbc4
pawan bhati <v-pawan.bhati@dmartindia.com>
2019-07-23 14:47:05 +0530

    android project

---
+++ app/src/main/java/com/pawan/ci_cd_example/MainActivity.java
@@ -0,0 +1,29 @@
+package com.pawan.ci_cd_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void startServiceDemo(View view) {
+
+    }
+
+    public void startBroadcastReceiverDemo(View view) {
+
+    }
+
+    public void startContentProviderDemo(View view) {
+
+    }
+
+    public void startBoundServiceDemo(View view) {
+
+    }
+}
